da6a44eb09c3b97b22b0841431a02b9a825c2af7
bl00dhound <bloodhound1982@gmail.com>
2018-09-10 07:58:10 +0300

    refactoring routes

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
-const environment = {};
-
-environment.staging = {
-	httpPort : 4000,
-	httpsPort: 4001,
-	env: 'staging'
+const environment = {
+	staging: {
+		httpPort: 4000,
+		httpsPort: 4001,
+		env: 'staging'
+	},
+	production: {
+		httpPort: 5000,
+		httpsPort: 5001,
+		env: 'production'
+	}
 };
 
-environment.production = {
-	httpPort : 5000,
-	httpsPort: 5001,
-	env: 'production'
-};
+const currentEnvironment = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
 
-const currentEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
-
-module.exports = typeof(environment[currentEnvironment]) === 'object' ? environment[currentEnvironment] : environment.staging;
+module.exports = typeof (environment[currentEnvironment]) === 'object' ? environment[currentEnvironment] : environment.staging;
